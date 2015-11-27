3ef9380995ad290698bdcec440a862579f445bdf
Klaus Silveira <contact@klaussilveira.com>
2015-11-27 01:35:13 -0200

    Migrated to PHP 7.

---
+++ src/Inflector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Linio\Component\Util;
+
+class Inflector
+{
+    public static function pascalize(string $string): string
+    {
+        return ucfirst(str_replace(' ', '', ucwords(strtr($string, '_-', '  '))));
+    }
+
+    public static function camelize(string $string): string
+    {
+        return lcfirst(self::pascalize($string));
+    }
+}
